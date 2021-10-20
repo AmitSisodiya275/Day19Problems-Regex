b3ac18607acefc1ea70be0f309860a25ac537557
AmitSisodiya275 <amitsisodiya275@gmail.com>
2021-10-20 19:31:52 +0530

    Added: all email test cases manually without the parameterized test case.

--- main/java/com/bridgelab/regex/NameValidator.java
+++ main/java/com/bridgelab/regex/NameValidator.java
@@ -6,7 +6,7 @@ public class NameValidator {
 
 	public static final String NAME_VALIDATOR = "^[A-Z]{1}[a-zA-z]{2,}$";
 	public static final String NUMBER_VALIDATOR = "^[0-9]{2}[ ]{1}[0-9]{10}$";
-	public static final String EMAIL_VALIDATIOR ="^[a-zA-Z0-9]+[._a-zA-Z0-9!#$%&'*+-/=?^_`{|}~]*[a-zA-Z]*@[a-zA-Z0-9]{2,8}.[a-zA-Z.]{2,6}";
+	public static final String EMAIL_VALIDATIOR ="^[a-zA-Z0-9]+[._a-zA-Z0-9!#$%&'*+-/=?^_`{|}~]*[a-zA-Z]*@[a-zA-Z0-9]{1,8}.[a-zA-Z.]{2,6}";
 	public static final String PASSWORD_VALIDATION = "(?=.*[A-Z])(?=.*[@#$%^&*-_+=]){1}(?=.*[0-9])(?=.*[a-z]).{8,}";
 	public boolean firstNameValidator(String firstName) {
 
--- test/java/com/bridgelab/regex/NameValidatorTest.java
+++ test/java/com/bridgelab/regex/NameValidatorTest.java
@@ -70,4 +70,123 @@ public class NameValidatorTest {
 		Boolean result = validator.passwordValidation("Amit5@sisodiya");
 		assertTrue(result);
 	}
+	
+	@Test 
+	public void whenEnteredFirstValidEmailShouldPassTheEmailTest() {
+		NameValidator validator = new NameValidator();
+		Boolean result = validator.emailValidator("abc@yahoo.com");
+		assertTrue(result);
+	}
+	
+	@Test 
+	public void whenEnteredSecondValidEmailShouldPassTheEmailTest() {
+		NameValidator validator = new NameValidator();
+		Boolean result = validator.emailValidator("abc-100@yahoo.com");
+		assertTrue(result);
+	}
+	
+	@Test 
+	public void whenEnteredThirdValidEmailShouldPassTheEmailTest() {
+		NameValidator validator = new NameValidator();
+		Boolean result = validator.emailValidator("abc.100@yahoo.com");
+		assertTrue(result);
+	}
+	
+	@Test 
+	public void whenEnteredFourthValidEmailShouldPassTheEmailTest() {
+		NameValidator validator = new NameValidator();
+		Boolean result = validator.emailValidator("abc-100@abc.net");
+		assertTrue(result);
+	}
+	
+	@Test 
+	public void whenEnteredFifthValidEmailShouldPassTheEmailTest() {
+		NameValidator validator = new NameValidator();
+		Boolean result = validator.emailValidator("abc.100@abc.com.au");
+		assertTrue(result);
+	}
+	
+	@Test 
+	public void whenEnteredSixthValidEmailShouldPassTheEmailTest() {
+		NameValidator validator = new NameValidator();
+		Boolean result = validator.emailValidator("abc@1.com");
+		assertTrue(result);
+	}
+	
+	@Test 
+	public void whenEnteredEighthValidEmailShouldPassTheEmailTest() {
+		NameValidator validator = new NameValidator();
+		Boolean result = validator.emailValidator("abc+100@gmail.com");
+		assertTrue(result);
+	}
+	
+	@Test 
+	public void whenEnteredFirstInValidEmailShouldReturnFalseAndPassTheEmailTest() {
+		NameValidator validator = new NameValidator();
+		Boolean result = validator.emailValidator("abc");
+		assertFalse(result);
+	}
+	
+	@Test 
+	public void whenEnteredSecondInValidEmailShouldReturnFalseAndPassTheEmailTest() {
+		NameValidator validator = new NameValidator();
+		Boolean result = validator.emailValidator("abc@.com.my");
+		assertFalse(result);
+	}
+	
+	@Test 
+	public void whenEnteredFouthInValidEmailShouldReturnFalseAndPassTheEmailTest() {
+		NameValidator validator = new NameValidator();
+		Boolean result = validator.emailValidator("abc123@.com");
+		assertFalse(result);
+	}
+	
+	@Test 
+	public void whenEnteredFifthInValidEmailShouldReturnFalseAndPassTheEmailTest() {
+		NameValidator validator = new NameValidator();
+		Boolean result = validator.emailValidator("abc123@.com.com");
+		assertFalse(result);
+	}
+	
+	@Test 
+	public void whenEnteredSixthInValidEmailShouldReturnFalseAndPassTheEmailTest() {
+		NameValidator validator = new NameValidator();
+		Boolean result = validator.emailValidator(".abc@abc.com");
+		assertFalse(result);
+	}
+	
+	@Test 
+	public void whenEnteredSeventhInValidEmailShouldReturnFalseAndPassTheEmailTest() {
+		NameValidator validator = new NameValidator();
+		Boolean result = validator.emailValidator("abc()*@gmail.com");
+		assertFalse(result);
+	}
+	
+	@Test 
+	public void whenEnteredEighthInValidEmailShouldReturnFalseAndPassTheEmailTest() {
+		NameValidator validator = new NameValidator();
+		Boolean result = validator.emailValidator("abc@%*.com");
+		assertFalse(result);
+	}
+	
+	@Test 
+	public void whenEnteredEleventhInValidEmailShouldReturnFalseAndPassTheEmailTest() {
+		NameValidator validator = new NameValidator();
+		Boolean result = validator.emailValidator("abc@abc@gmail.com");
+		assertFalse(result);
+	}
+	
+	@Test 
+	public void whenEnteredTwelvethInValidEmailShouldReturnFalseAndPassTheEmailTest() {
+		NameValidator validator = new NameValidator();
+		Boolean result = validator.emailValidator("abc@gmail.com.1a");
+		assertFalse(result);
+	}
+	
+	@Test 
+	public void whenEnteredThirteenthInValidEmailShouldReturnFalseAndPassTheEmailTest() {
+		NameValidator validator = new NameValidator();
+		Boolean result = validator.emailValidator("abc@gmail.com.aa.au");
+		assertFalse(result);
+	}
 }
